ef947ecfa1550c2c4674d101d69a50195c82b2da
Dima Kos <svinuga@gmail.com>
2023-09-29 09:23:28 +0400

    correct xaml errors

--- Bookshelf/MainWindow.xaml.cs
+++ Bookshelf/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Bookshelf
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
